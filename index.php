cdd9a324f496fb1d0a2e8bbb113aa5ff9dcfa164
i80586 <i80586@mail.ru>
2014-01-07 00:50:03 +0400

    commented including classmap file

--- index.php
+++ index.php
@@ -6,5 +6,6 @@ define('FRAMEWORK_DIR', BASE_PATH . '/framework/');
 require FRAMEWORK_DIR . 'core/Application.php';
 
 $app = new \SS\Application(require BASE_PATH . '/config/default.php');
-\SS\Application::$classmap = require BASE_PATH . '/config/classMap.php';
+// open comment if you want to include classmap file to project
+// \SS\Application::$classmap = require BASE_PATH . '/config/classMap.php';
 $app->start();
