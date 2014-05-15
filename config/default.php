8d987717071ae9503b51641879842bf53804c08a
i80586 <i80586@mail.ru>
2014-05-15 22:35:58 +0500

    changed default database name

--- config/default.php
+++ config/default.php
@@ -12,7 +12,7 @@ return [
 	
 	 /* Clear comments if you'll use database
 	'db' => [
-		'dsn' => 'mysql:host=localhost;dbname=mestero;',
+		'dsn' => 'mysql:host=localhost;dbname=test;',
 		'username' => 'root',
 		'password' => '',
 		'encoding' => 'utf8'
