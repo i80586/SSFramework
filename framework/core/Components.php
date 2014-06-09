ba2fd8488e04ace76e103af8bae5bd77b4d8b017
i80586 <i80586@mail.ru>
2014-06-09 16:17:16 +0500

    refactoring class

--- framework/core/Components.php
+++ framework/core/Components.php
@@ -12,71 +12,77 @@ namespace framework\core;
 class Components
 {
 
-    /**
+	/**
 	 * Components list
-     * @var array
-     */
-    protected static $_componentsList = [];
-
-    /**
-     * Components paths list
-     * @var array
-     */
-    protected static $_components = [];
+	 * 
+	 * @var array
+	 */
+	protected static $_componentsList = [];
 
 	/**
-     * Get component. Create new if not exists
-     * @param string $name
-	 * @param array $arguments
-     * @return object
-     */
-    public static function getComponent($name, array $arguments)
-    {
-        if (!isset(self::$_componentsList[$classPath = self::getComponentPath($name)])) {
-            self::registerComponent($classPath, $arguments);
-            self::createComponent($name, $classPath);
-        }
-        return self::$_componentsList[$classPath];
-    }
+	 * Components paths list
+	 * 
+	 * @var array
+	 */
+	protected static $_components = [];
 
-    /**
-     * Create component
-     * @param string $name
+	/**
+	 * Get component. Create new if not exists
+	 * 
+	 * @param string $name
+	 * @param array $arguments
+	 * @return object
+	 */
+	public static function getComponent($name, array $arguments)
+	{
+		if (!isset(self::$_componentsList[$classPath = self::getComponentPath($name)])) {
+			self::registerComponent($classPath, $arguments);
+			self::createComponent($name, $classPath);
+		}
+		return self::$_componentsList[$classPath];
+	}
+
+	/**
+	 * Create component
+	 * 
+	 * @param string $name
 	 * @param string $path
 	 * @throws \framework\core\Exception
-     */
-    protected static function createComponent($name, $path)
-    {
-        if (!isset(self::$_components[$path])) {
-            throw new \framework\core\Exception('Undefined component: ' . $name);
-        }
-		
+	 */
+	protected static function createComponent($name, $path)
+	{
+		if (!isset(self::$_components[$path])) {
+			throw new \framework\core\Exception('Undefined component: ' . $name);
+		}
+
 		try {
 			$reflectionClass = new \ReflectionClass($path);
 		} catch (\ReflectionException $e) {
 			throw new Exception('Component <b>:c</b> doesn\'t exists at path <b>:p</b>', [
-					':c' => $name,
-					':p' => $path
-				]);
+				':c' => $name,
+				':p' => $path
+			]);
 		}
-        
-        self::$_componentsList[$path] = empty(self::$_components[$path]) ?
-                $reflectionClass->newInstance() :
-                $reflectionClass->newInstance(self::$_components[$path]);
-    }
 
-    /**
-     * Register new component
-     * @param string $name
-     * @param array $params
-     */
-    protected static function registerComponent($name, array $params = [])
-    {
-        self::$_components[$name] = $params;
-    }
+		self::$_componentsList[$path] = empty(self::$_components[$path]) ?
+				$reflectionClass->newInstance() :
+				$reflectionClass->newInstance(self::$_components[$path]);
+	}
+
+	/**
+	 * Register new component
+	 * 
+	 * @param string $name
+	 * @param array $params
+	 */
+	protected static function registerComponent($name, array $params = [])
+	{
+		self::$_components[$name] = $params;
+	}
 
 	/**
 	 * Get component path
+	 * 
 	 * @param string $name
 	 * @return string
 	 */
@@ -87,4 +93,5 @@ class Components
 		}
 		return Application::getConfig()['components'][$name]['path'];
 	}
+
 }
