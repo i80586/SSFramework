bec739651e3a5d2b111b4682ac5cb26beeb612e5
i80586 <i80586@mail.ru>
2014-06-21 15:12:56 +0500

    check for file existing

--- framework/core/SplClassLoader.php
+++ framework/core/SplClassLoader.php
@@ -148,8 +148,10 @@ class SplClassLoader
                 $fileName = str_replace($this->_namespaceSeparator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
             }
             $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . $this->_fileExtension;
-
-            require ($this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '') . $fileName;
+			
+			if (is_file($filePath = ($this->_includePath !== null ? $this->_includePath . DIRECTORY_SEPARATOR : '') . $fileName)) {
+				require $filePath;
+			}
         }
     }
 }
